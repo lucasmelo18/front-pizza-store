541c186b62ee665cfb67b871179072242d5e76eb
Lucas Melo <luucasmell@gmail.com>
2019-08-08 23:20:45 -0300

    Adicionar metodos de get e add no constructor do department service

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const departmentController = require('./departmentController');
 const app = express();
 
 app.use(bodyParser.json());
--- server/departmentController.js
+++ server/departmentController.js
@@ -26,3 +26,5 @@ router.get('/', function(request, response){
         }
     })
 })
+
+module.exports = router;
