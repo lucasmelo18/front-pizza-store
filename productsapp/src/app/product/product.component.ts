c08973512791832f6bfd532f4d189bde823b6f8a
Lucas Melo <luucasmell@gmail.com>
2019-08-10 11:31:51 -0300

    Constrir lista de produtos no componentss

--- productsapp/src/app/product/product.component.ts
+++ productsapp/src/app/product/product.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Product } from '../product';
+import { DepartmentService } from '../department.service';
+import { Department } from '../department';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product',
@@ -8,10 +14,35 @@ import { ProductService } from '../product.service';
 })
 export class ProductComponent implements OnInit {
 
-  constructor(private productsService: ProductService) { }
+  productFrom: FormGroup = this.fb.group({
+    _id:[null],
+    name: ['', [Validators.required]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    departments: [[], [Validators.required]],
+
+  })
+
+  products: Product[] = [];
+  departments: Department[] = [];
+
+  private unsubscribe: Subject<any> = new Subject<any>();
+
+  constructor(
+    private productsService: ProductService,
+    private fb: FormBuilder,
+    private departmentService: DepartmentService) { }
 
   ngOnInit() {
-    this.productsService.get().subscribe((prod) => console.log(prod));
+    this.productsService.get()
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe((prods) => this.products = prods);
+    this.departmentService.get()
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe((deps) => this.departments = deps)
   }
 
+  ngOnDestroy(){
+    this.unsubscribe.next();
+  }
 }
