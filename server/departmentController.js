dda950b92a5ac9124e434df8e6f6cef42b0bc73c
Lucas Melo <luucasmell@gmail.com>
2019-08-10 00:19:04 -0300

    Adicionar rota de delete

--- server/departmentController.js
+++ server/departmentController.js
@@ -27,4 +27,33 @@ router.get('/', function(request, response){
     })
 })
 
+router.delete('/:id', (req, res) =>{
+    let id = req.params.id;
+    Department.deleteOne({id: id}, (err) =>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(200).send({});
+        }
+    })
+})
+
+router.patch('/:id', (req, res) =>{
+    Department.findById(req.params.id, (err, dep) =>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else if(!dep){
+            res.status(404).send({});
+        }
+        else{
+            dep.name = req.body.name;
+            dep.save()
+                .then((d) => res.status(200).send(d))
+                .catch((e) => res.status(500).send(e));
+        }
+    })
+})
+
 module.exports = router;
