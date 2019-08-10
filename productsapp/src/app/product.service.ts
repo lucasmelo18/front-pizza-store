f011730ac106aed924dc907767f3a7f90d42ce7e
Lucas Melo <luucasmell@gmail.com>
2019-08-10 01:52:54 -0300

    Atrelar os produtos aos deparments atraves do metodo de get

--- productsapp/src/app/product.service.ts
+++ productsapp/src/app/product.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest } from 'rxjs';
 import { Product } from './product';
 import { HttpClient } from '@angular/common/http';
+import { DepartmentService } from './department.service';
+import { map, tap, filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,16 +14,32 @@ export class ProductService {
   private productsSubjects: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>(null);
   private loaded: boolean = false;
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private departmentService: DepartmentService ) {
+      this.get().subscribe((prod) => console.log(prod))
+     }
 
-  get(): Observable<Product[]>{
-    if(!this.loaded){
-      this.http.get<Product[]>(this.url)
-        .subscribe(this.productsSubjects);
+  get(): Observable<Product[]> {
+    if (!this.loaded) {
+      combineLatest(
+        this.http.get<Product[]>(this.url),
+        this.departmentService.get())
+      .pipe(
+        map(([products,departments])=> {
+          for(let p of products) {
+            let ids = (p.departments as string[]);
+            p.departments = ids.map((id)=>departments.find(dep=>dep._id==id));
+          }
+          return products;
+        }),
+        tap((products) => console.log(products))
+      )
+      .subscribe(this.productsSubjects);
+
       this.loaded = true;
     }
-    
     return this.productsSubjects.asObservable();
-
   }
+
 }
--- productsapp/src/app/product.ts
+++ productsapp/src/app/product.ts
@@ -2,7 +2,7 @@ import { Department } from './department';
 
 export interface Product {
     name: String;
-    departments: Department[];
+    departments: Department[] | string[];
     stock: Number;
     price: Number;
     _id ?: String;
