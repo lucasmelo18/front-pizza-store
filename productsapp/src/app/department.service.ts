3816a239583e429ea1650ab2b8f1a90b65d53363
Lucas Melo <luucasmell@gmail.com>
2019-08-10 00:13:01 -0300

    Corrigir service para atualizar lista ao adicionar item em tempo real

--- productsapp/src/app/department.service.ts
+++ productsapp/src/app/department.service.ts
@@ -1,22 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Department } from './department';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
 
-  readonly  url = 'http://localhost:3000/areas'
+  readonly  url = 'http://localhost:3000/areas';
+
+  private departmentsSubjects: BehaviorSubject<Department[]> =  new BehaviorSubject<Department[]>(null);
+  private loaded : boolean = false;
 
   constructor(private http: HttpClient) { }
 
   get(): Observable<Department[] >{
-    return this.http.get<Department[]>(this.url);
+    if(!this.loaded){
+      this.http.get<Department[]>(this.url)
+        .pipe(tap((deps) => console.log(deps)))
+        .subscribe(this.departmentsSubjects);
+      this.loaded = true;
+    }
+
+    return this.departmentsSubjects.asObservable();
   }
 
   add(d: Department): Observable<Department>{
-     return this.http.post<Department>(this.url, d);
+     return this.http.post<Department>(this.url, d)
+     .pipe(tap((dep: Department) => this.departmentsSubjects.getValue().push(dep)))
   }
 }
