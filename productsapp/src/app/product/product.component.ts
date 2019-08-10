e33cf48aae6b44fe61ea8636f128e2a92cbc0ef6
Lucas Melo <luucasmell@gmail.com>
2019-08-10 12:42:58 -0300

    Adicionar metodo de deletar e editar produtos

--- productsapp/src/app/product/product.component.ts
+++ productsapp/src/app/product/product.component.ts
@@ -6,6 +6,7 @@ import { DepartmentService } from '../department.service';
 import { Department } from '../department';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-product',
@@ -15,7 +16,7 @@ import { takeUntil } from 'rxjs/operators';
 export class ProductComponent implements OnInit {
 
   productForm: FormGroup = this.fb.group({
-    _id:[null],
+    _id: [null],
     name: ['', [Validators.required]],
     stock: [0, [Validators.required, Validators.min(0)]],
     price: [0, [Validators.required, Validators.min(0)]],
@@ -31,7 +32,8 @@ export class ProductComponent implements OnInit {
   constructor(
     private productsService: ProductService,
     private fb: FormBuilder,
-    private departmentService: DepartmentService) { }
+    private departmentService: DepartmentService,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit() {
     this.productsService.get()
@@ -42,26 +44,39 @@ export class ProductComponent implements OnInit {
       .subscribe((deps) => this.departments = deps)
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.unsubscribe.next();
   }
 
-  save(){
+  save() {
     let data = this.productForm.value;
     if (data._id != null) {
       this.productsService.update(data)
-        .subscribe(        )
+        .subscribe()
     }
     else {
       this.productsService.add(data)
         .subscribe();
     }
   }
-  delete(p: Product){
-
+  delete(p: Product) {
+    this.productsService.del(p)
+      .subscribe(
+        () => {
+          this.notify('Deletado com sucesso'),
+          (err) => {
+            console.error(err);
+            this.notify('Erro ao deletar, tente novamente')
+          }
+        }
+      )
   }
 
-  edit(p: Product){
+  edit(p: Product) {
+    this.productForm.setValue(p);
+  }
 
+  notify(msg: string) {
+    this.snackBar.open(msg, 'OK', { duration: 3000 })
   }
 }
