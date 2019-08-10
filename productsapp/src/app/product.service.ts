70df6f3e3ab4a2ad6615c23b3d4203ee5ab6a660
Lucas Melo <luucasmell@gmail.com>
2019-08-10 01:39:28 -0300

    Adicionar metodo d get no service de procuts

--- productsapp/src/app/product.service.ts
+++ productsapp/src/app/product.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Product } from './product';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  readonly url = 'http://localhost:3000/sabores';
+  private productsSubjects: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>(null);
+  private loaded: boolean = false;
+
+  constructor(private http: HttpClient) { }
+
+  get(): Observable<Product[]>{
+    if(!this.loaded){
+      this.http.get<Product[]>(this.url)
+        .subscribe(this.productsSubjects);
+      this.loaded = true;
+    }
+    
+    return this.productsSubjects.asObservable();
+
+  }
 }
